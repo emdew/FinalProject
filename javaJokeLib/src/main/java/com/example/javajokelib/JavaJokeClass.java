df6735201efda57fcaf1433cddaf734d0f78b7f9
emdew <ed139509@gmail.com>
2018-09-25 21:33:08 +0900

    Initial commit

---
+++ javaJokeLib/src/main/java/com/example/javajokelib/JavaJokeClass.java
@@ -0,0 +1,7 @@
+package com.example.javajokelib;
+
+public class JavaJokeClass {
+    public String getJoke() {
+        return "What's the difference between a good joke and a bad joke timing.";
+    }
+}
---
+++ myandroidlibrary/src/main/java/com/example/myandroiodlibrary/JokeActivityFragment.java
@@ -0,0 +1,28 @@
+package com.example.myandroiodlibrary;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.javajokelib.JavaJokeClass;
+
+public class JokeActivityFragment extends Fragment {
+
+    public JokeActivityFragment(){}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        String resultOfAsync = getArguments().getString("result");
+
+        View rootView = inflater.inflate(R.layout.joke_fragment, container, false);
+
+        TextView textView = (TextView) rootView.findViewById(R.id.text_view);
+        textView.setText(resultOfAsync);
+
+        return rootView;
+    }
+}
