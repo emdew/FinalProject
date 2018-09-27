7f2b11672e58baf3988aaf47b4b5e3095948e0bf
emdew <ed139509@gmail.com>
2018-09-27 12:50:10 +0900

    trouble with the paid flavor -- error duplicate class

--- app/src/free/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -19,6 +19,7 @@ import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
 import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
 import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+import com.udacity.gradle.builditbigger.R;
 import com.udacity.gradle.builditbigger.backend.myApi.MyApi;
 
 import java.io.IOException;
--- app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -6,9 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
 
+import com.udacity.gradle.builditbigger.R;
 
 /**
  * A placeholder fragment containing a simple view.
